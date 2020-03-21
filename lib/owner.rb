301d96052fbd19c0f64d6e42f60ad5ba7d5da3d6
Charles Adkins <cpeadkins@gmail.com>
2020-03-21 21:39:44 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -25,4 +25,43 @@ class Owner
     @@all.clear
   end
   
+##################################################  
+  
+   def cats
+    Cat.all.select {|cat| cat.owner == self}
+  end
+
+  def dogs
+    Dog.all.select {|dog| dog.owner == self}
+  end
+
+  def buy_cat(name)
+    cat = Cat.new(name, self)
+  end
+
+  def buy_dog(name)
+    dog = Dog.new(name, self)
+  end
+
+  def walk_dogs
+    Dog.all.map {|dog| dog.mood = "happy"}
+  end
+
+  def feed_cats
+    Cat.all.map {|cat| cat.mood = "happy"}
+  end
+
+  def sell_pets
+    Cat.all.each {|cat| cat.mood = "nervous" }
+    Dog.all.each {|dog| dog.mood = "nervous" }
+    Cat.all.each {|cat| cat.owner = nil }
+    Dog.all.each {|dog| dog.owner = nil }
+  end
+
+  def list_pets
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end
+ 
+##################################################  
+  
 end
