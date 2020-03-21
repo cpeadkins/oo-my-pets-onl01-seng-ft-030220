a307092e20943a40937e4a42ed67f02b10375679
Charles Adkins <cpeadkins@gmail.com>
2020-03-21 21:37:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,8 +7,15 @@ class Dog
     @name = name
     @owner = owner
     @mood = "nervous"
+    save
   end
   
-  def 
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
   
 end
